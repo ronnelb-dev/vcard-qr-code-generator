4dbadfa08068d9a3dae4999b1c126c7ad5762466
Ronnel Barashari <barasharironnel29@gmail.com>
2025-03-11 16:57:36 +0800

    vcard first push

---
+++ app/ui/business-form.tsx
@@ -0,0 +1,280 @@
+"use client";
+import React, { useRef, useState } from "react";
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import { QRCodeCanvas } from "qrcode.react";
+
+export default function BusinessForm() {
+  const qrRef = useRef<HTMLDivElement | null>(null);
+  const [contact, setContact] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    company: "",
+    jobTitle: "",
+    url: "",
+    address: "",
+  });
+
+  const [qrValue, setQrValue] = useState("");
+
+  const generateVCard = () => {
+    const vCardData = `BEGIN:VCARD\nVERSION:3.0\nN:${contact.lastName};${contact.firstName}\nFN:${contact.firstName} ${contact.lastName}\nORG:${contact.company}\nTITLE:${contact.jobTitle}\nTEL:${contact.phone}\nEMAIL:${contact.email}\nURL:${contact.url}\nADR;TYPE=WORK:;;${contact.address}\nEND:VCARD`;
+    setQrValue(vCardData);
+  };
+
+  const downloadQRCode = () => {
+    if (qrRef.current instanceof HTMLDivElement) {
+      const canvas = qrRef.current.querySelector("canvas");
+      if (canvas instanceof HTMLCanvasElement) {
+        const link = document.createElement("a");
+        link.href = canvas.toDataURL("image/png");
+        link.download = "vcard_qr_code.png";
+        link.click();
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12 text-center">
+          <h2 className="text-base/7 font-semibold text-gray-900">
+            vCard QR Code Generator
+          </h2>
+        </div>
+
+        <div className="border-b border-gray-900/10">
+          <h2 className="text-base/7 font-semibold text-gray-900">
+            Business Information
+          </h2>
+          {/* <p className="mt-1 text-sm/6 text-gray-600">
+            This information will be used to generate a QR code, so share it
+            carefully.
+          </p> */}
+
+          <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  onChange={(e) =>
+                    setContact({ ...contact, firstName: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  onChange={(e) =>
+                    setContact({ ...contact, lastName: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Company
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  onChange={(e) =>
+                    setContact({ ...contact, company: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Job
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  onChange={(e) =>
+                    setContact({ ...contact, jobTitle: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="country"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Phone
+              </label>
+              <div className="mt-2 grid grid-cols-1">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                >
+                  <option>Mobile</option>
+                  <option>Work</option>
+                  <option>Landline</option>
+                  <option>Home</option>
+                </select>
+                <ChevronDownIcon
+                  aria-hidden="true"
+                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="last-name"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Number
+              </label>
+              <div className="mt-2">
+                <input
+                  id="number"
+                  name="number"
+                  type="number"
+                  autoComplete="number"
+                  onChange={(e) =>
+                    setContact({ ...contact, phone: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  onChange={(e) =>
+                    setContact({ ...contact, email: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                URL/Website
+              </label>
+              <div className="mt-2">
+                <input
+                  id="url"
+                  name="url"
+                  type="url"
+                  onChange={(e) =>
+                    setContact({ ...contact, url: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="address"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="address"
+                  name="address"
+                  type="text"
+                  autoComplete="address"
+                  onChange={(e) =>
+                    setContact({ ...contact, address: e.target.value })
+                  }
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-center gap-x-6">
+        <button
+          onClick={generateVCard}
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          GENERATE QR CODE
+        </button>
+      </div>
+
+      {qrValue && (
+        <div ref={qrRef} className="p-4 border rounded-md shadow-md mt-6">
+          <QRCodeCanvas value={qrValue} size={420} />
+        </div>
+      )}
+      {qrValue && (
+        <div className="mt-6 flex items-center justify-center gap-x-6">
+          <button
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={downloadQRCode}
+          >
+            Download QR Code
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
